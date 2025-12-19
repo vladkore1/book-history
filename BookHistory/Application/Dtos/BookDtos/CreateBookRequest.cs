9111345cdaa2d8a7c3a16efaa57ddaa3f33d6baf
Vlad Ceaus <vlad.ceaus@gmail.com>
2025-12-19 16:27:01 +0100

    Improve book request validation

--- BookHistory/Application/Dtos/BookDtos/CreateBookRequest.cs
+++ BookHistory/Application/Dtos/BookDtos/CreateBookRequest.cs
@@ -1,10 +1,18 @@
-﻿namespace BookHistory.Application.Dtos.BookDtos
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BookHistory.Application.Dtos.BookDtos
 {
     public sealed class CreateBookRequest
     {
-        public required string Title { get; init; }
-        public required string Description { get; init; }
-        public required DateOnly PublishDate { get; init; }
-        public required List<string> Authors { get; init; }
+        [Required]
+        [MaxLength(200)]
+        public string Title { get; init; } = String.Empty;
+        [Required]
+        [MaxLength(1000)]
+        public string Description { get; init; } = String.Empty;
+        [Required]
+        public DateOnly PublishDate { get; init; }
+        [Required]
+        public List<string> Authors { get; init; } = [];
     }
 }
